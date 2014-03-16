7cccfab1d716816f3adc4bd7f7e95731ebc9f490
Craig Anderson <craig@cander.me>
2014-03-16 11:19:49 +0000

    removed the big day route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,10 +12,6 @@ angular.module('weddingApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/bigDay', {
-        templateUrl: 'views/bigday.html',
-        controller: 'BigdayCtrl'
-      })
       .when('/save-the-date', {
           templateUrl: 'views/save-the-date.html',
           controller: 'SaveTheDateCtrl'
